de0816050da4096fb7547840d391c66d71ca2b5f
Oleksandr HOLIADYNETS <sanyagal191919@gmail.com>
2025-12-08 00:30:11 +0000

    w kodzie ty zmieniasz ale wizualnie nic nie zmienia sie

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -27,10 +27,7 @@ function Calendar({
   return (
     <DayPicker
       showOutsideDays={showOutsideDays}
-      className={cn(
-        "p-3",
-        className
-      )}
+      className={cn("p-3", className)}
       classNames={{
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
@@ -107,7 +104,6 @@ function Calendar({
         },
       }}
       {...props}
-      locale={pl}
     />
   )
 }
