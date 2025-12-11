190b783fa96b895240a8480c9e7145afbabc13ff
Oleksandr HOLIADYNETS <sanyagal191919@gmail.com>
2025-12-11 19:54:40 +0000

    Myśli jako zespół składany z : Senior Web developer,Backend Developer,Fr

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,16 +1,13 @@
+"use client"
 
-"use client";
+import * as React from "react"
+import { ChevronLeft, ChevronRight } from "lucide-react"
+import { DayPicker } from "react-day-picker"
 
-import * as React from "react";
-import { ChevronLeft, ChevronRight } from "lucide-react";
-import { DayPicker, useDayPicker, useNavigation } from "react-day-picker";
-import { format } from "date-fns";
-import { pl } from "date-fns/locale";
+import { cn } from "@/lib/utils"
+import { buttonVariants } from "@/components/ui/button"
 
-import { cn } from "@/lib/utils";
-import { buttonVariants } from "@/components/ui/button";
-
-export type CalendarProps = React.ComponentProps<typeof DayPicker>;
+export type CalendarProps = React.ComponentProps<typeof DayPicker>
 
 function Calendar({
   className,
@@ -26,7 +23,7 @@ function Calendar({
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
         caption: "flex justify-center pt-1 relative items-center",
-        caption_label: "text-lg font-bold",
+        caption_label: "text-sm font-medium",
         nav: "space-x-1 flex items-center",
         nav_button: cn(
           buttonVariants({ variant: "outline" }),
@@ -39,15 +36,17 @@ function Calendar({
         head_cell:
           "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
         row: "flex w-full mt-2",
-        cell: "h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
+        cell: "h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
         day: cn(
           buttonVariants({ variant: "ghost" }),
           "h-9 w-9 p-0 font-normal aria-selected:opacity-100"
         ),
+        day_range_end: "day-range-end",
         day_selected:
           "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
         day_today: "bg-accent text-accent-foreground",
-        day_outside: "text-muted-foreground opacity-50",
+        day_outside:
+          "day-outside text-muted-foreground opacity-50 aria-selected:bg-accent/50 aria-selected:text-muted-foreground",
         day_disabled: "text-muted-foreground opacity-50",
         day_range_middle:
           "aria-selected:bg-accent aria-selected:text-accent-foreground",
@@ -55,62 +54,13 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        Months: () => {
-          const { goToMonth, nextMonth, previousMonth } = useNavigation();
-          const { currentMonth } = useDayPicker();
-
-          const weekdays = ["pon", "wto", "śro", "czw", "pią", "sob", "nie"];
-
-          return (
-            <div className="grid grid-cols-[auto_1fr] gap-x-4">
-              <div className="flex flex-col justify-center items-center gap-4">
-                <button
-                  type="button"
-                  disabled={!previousMonth}
-                  onClick={() => previousMonth && goToMonth(previousMonth)}
-                  className={cn(
-                    buttonVariants({ variant: "outline", size: "icon" }),
-                    "h-7 w-7 rounded-md"
-                  )}
-                >
-                  <ChevronLeft className="h-4 w-4" />
-                </button>
-                <button
-                  type="button"
-                  disabled={!nextMonth}
-                  onClick={() => nextMonth && goToMonth(nextMonth)}
-                  className={cn(
-                    buttonVariants({ variant: "outline", size: "icon" }),
-                    "h-7 w-7 rounded-md"
-                  )}
-                >
-                  <ChevronRight className="h-4 w-4" />
-                </button>
-              </div>
-
-              <div className="grid grid-cols-1">
-                <div className="grid grid-cols-7 gap-1">
-                  <div className="col-span-7 text-lg font-bold text-center">
-                    {currentMonth &&
-                      format(currentMonth, "LLLL yyyy", { locale: pl })}
-                  </div>
-
-                  <div className="font-bold text-center">{weekdays[0]}</div>
-                  <div className="col-span-6 text-center font-bold">
-                    {weekdays.slice(1).join(" ")}
-                  </div>
-                </div>
-                {props.children}
-              </div>
-            </div>
-          );
-        },
+        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
+        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
       }}
-      locale={pl}
       {...props}
     />
-  );
+  )
 }
-Calendar.displayName = "Calendar";
+Calendar.displayName = "Calendar"
 
-export { Calendar };
+export { Calendar }
