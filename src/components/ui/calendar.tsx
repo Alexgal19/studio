12d3f3f123dae50eb9d569b861f05060a09290f3
Oleksandr HOLIADYNETS <sanyagal191919@gmail.com>
2025-12-11 14:47:57 +0000

    ?

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -32,7 +32,7 @@ function Calendar({
         caption: "flex justify-center pt-1 relative items-center",
         caption_label: "text-sm font-medium",
         caption_dropdowns: "flex justify-center gap-2",
-        nav: "space-x-1 flex items-center",
+        nav: "space-x-1 flex items-center absolute left-1",
         nav_button: cn(
           buttonVariants({ variant: "outline" }),
           "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100"
