64eb04ad8c0d94fd2e138d33d78b3b5b3bff45e0
Oleksandr HOLIADYNETS <sanyagal191919@gmail.com>
2025-12-11 20:39:17 +0000

    super ale usun niepotrzebne dane ,popatrz na zdjecie

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -30,7 +30,7 @@ function Calendar({
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
         caption: "flex justify-start pt-1 relative items-center",
-        caption_label: "text-lg font-bold",
+        caption_label: "text-lg font-bold sr-only",
         caption_dropdowns: "flex gap-2",
         nav: "space-x-1 flex items-center ml-auto",
         nav_button: cn(
