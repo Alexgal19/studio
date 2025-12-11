f4bb89ebb2349557ae35a230c5393c886e49aa54
Oleksandr HOLIADYNETS <sanyagal191919@gmail.com>
2025-12-11 09:21:56 +0000

    nic nie dziala ,widok calendarza jest zorstko zakodowany,wkleilem calkow

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function forceRevalidate() {
+  // This function is intentionally left blank.
+  // Its presence is to force Next.js to re-evaluate its cache.
+}
